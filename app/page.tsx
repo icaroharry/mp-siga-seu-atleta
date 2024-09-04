591db601d3d73d77c76c04c87eba24ae471c6d09
Ícaro Harry <me@icaro.io>
2024-09-04 15:03:42 -0300

    Finish card

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,18 @@
-import db from "@/prisma/db";
+import AthletesList from "@/app/_components/athletes-list";
+import { findAthletes } from "@/lib/athletes";
+import { findSports } from "@/lib/sports";
+import { Suspense } from "react";
 
 export default async function Home() {
-  return <div></div>;
+  const athletes = await findAthletes();
+
+  const sports = await findSports();
+
+  return (
+    <main className="p-4 flex flex-col gap-12">
+      <Suspense key={"qualquer-valor"} fallback={<div>Carregando...</div>}>
+        <AthletesList initialData={athletes} />
+      </Suspense>
+    </main>
+  );
 }
